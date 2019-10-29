dc72872c519963e2dc0187c306f7ef6b38758489
Lucas de Carvalho <lucasdecarvalho.ti@gmail.com>
2019-10-29 08:40:40 -0300

    Finished class 'Validaçaõ de Agendamento'

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import ProviderController from './app/controllers/ProviderController';
 import SessionsController from './app/controllers/SessionController';
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
@@ -19,6 +20,7 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
