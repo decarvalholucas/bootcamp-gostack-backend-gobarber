4cbec820e9063cf7c1533bb5da611826148c6c14
Lucas de Carvalho Alves <lucasdecarvalho.ti@gmail.com>
2019-11-04 16:52:51 -0200

    Finished class configurando templates de e-mail

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,15 +1,38 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import expresshbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
 import mailConfig from '../config/mail';
 
 class Mail {
   constructor() {
     const { host, port, secure, auth } = mailConfig;
+
     this.transporter = nodemailer.createTransport({
       host,
       port,
       secure,
       auth: auth.user ? auth : null
     });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const templatePath = resolve(__dirname, '..', 'app', 'views', 'emails');
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: expresshbs.create({
+          layoutsDir: resolve(templatePath, 'layouts'),
+          partialsDir: resolve(templatePath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs'
+        }),
+        viewPath: templatePath,
+        extName: '.hbs'
+      })
+    );
   }
 
   sendMail(message) {
